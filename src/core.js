29d368300777bbaa2ec5f7b3b9cc9eff64adac8c
elusive avenger <reboot@armagames.org>
2016-08-31 00:48:40 +0500

    progress

--- src/core.js
+++ src/core.js
@@ -1,5 +1,58 @@
+const util = require('util');
+
+const express = require('express');
+
+const get = require('./get');
+
+const log = util.debuglog('eruditor-next-api');
+const app = express();
+
 exports.run = () => {
-  process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
+  process.argv.forEach((val, index) => {
+    log('%d: %s', index, val);
+  });
+
+  const types = [];
+
+  for (const key in get) {
+    if ({}.hasOwnProperty.call(get, key) && typeof get[key] === 'function') {
+      log('key: %s', key);
+      types.push(key);
+    }
+  }
+
+  app.use((req, res, next) => {
+    const origin = (req && req.headers && req.headers.origin)
+      || '*';
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
+
+  app.get('/api', (req, res) => {
+    res.json({
+      queryFormal: '/api/:type',
+      types,
+    });
+  });
+
+  app.get('/api/:type', (req, res) => {
+    const request = get[req.params.type];
+    if (typeof (request) === 'function') {
+      request(res);
+    } else {
+      res.json(
+        {
+          error: {
+            text: 'обработчик для данного запроса не установлен',
+          },
+        }
+      );
+    }
+  });
+
+  app.listen(27099, () => {
+    log('listening port 27099');
   });
 };
---
+++ src/get.js
@@ -0,0 +1,3 @@
+exports.fact = (res) => {
+  res.json({ type: 'fact' });
+};
